20232f535b4bd9e44330ea7f482d82a03e4de3e4
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-28 15:10:20 +0100

    [UPD] creato array oggetti 'biciclette'

---
+++ js/style.js
@@ -0,0 +1,42 @@
+const biciclette = [
+    {
+        nome: 'Giant TCR Advanced SL 0',
+        peso: 6.4,
+    },
+    {
+        nome: 'Scott Addict RC Ultimate',
+        peso: 5.9,
+    },    
+    {
+        nome: 'Specialized S-Works Aethos/Sram Red Etap AXS',
+        peso: 6.34,
+    },   
+    {
+        nome: 'Wilier Triestina Verticale SLR',
+        peso: 6.5,
+    },    
+    {
+        nome: 'Orbea Orca M10iLTD Replica',
+        peso: 6.7,
+    },    
+    {
+        nome: 'Look 795 Blade RS',
+        peso: 7,
+    },    
+    {
+        nome: 'Cube Litening Air C:68X SLT',
+        peso: 6.7,
+    },    
+    {
+        nome: 'Bianchi Specialissima RC/Shimano Dura-Ace',
+        peso: 6.6,
+    },    
+    {
+        nome: 'Cannondale SuperSix Evo Lab 71',
+        peso: 6.8,
+    },   
+    {
+        nome: 'Bmc Teammachine SLR 01 One',
+        peso: 6.7,
+    }
+]
